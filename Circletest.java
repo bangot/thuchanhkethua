34d934ed59ba648547d0ce0d2bafdaed3a034e2b
bangot <dangvanbang1802@gmail.com>
2020-11-11 16:54:50 +0700

    lamtiep

---
+++ Circletest.java
@@ -0,0 +1,10 @@
+public class Circletest {
+    public static void main(String[] args) {
+        Circle circle= new Circle();
+        System.out.println(circle);
+        circle =new Circle(3.5);
+        System.out.println(circle);
+        circle=new Circle("indogo", false, 3.5);
+        System.out.println(circle);
+    }
+}
